7c266ad97e9dd21be696af3fc2534f7970c5c0f6
Juan Benavides <jbbenavidesr@unal.edu.co>
2022-05-06 08:57:47 -0500

    :construction: Translate basic nav texts based on german.

--- src/i18n/es/nav.ts
+++ src/i18n/es/nav.ts
@@ -1,6 +1,30 @@
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
-	startHere: 'Configuración',
+	// Start Here
+	startHere: 'Comience aquí',
 	'getting-started': 'Empezando',
+	install: 'Instalación',
+	'editor-setup': 'Configuración del editor',
+	'comparing-astro-vs-other-tools': 'Astro vs. X',
+	migrate: 'Migrar',
+
+	// Core Concepts
+	coreConcepts: 'Conceptos principales',
+	'core-concepts/partial-hydration': 'Hidratación parcial',
+
+	// Basics
+	basics: 'Lo básico',
+	'core-concepts/project-structure': 'Estructura de un proyecto',
+	'core-concepts/astro-components': 'Componentes',
+	'core-concepts/astro-pages': 'Páginas',
+	'core-concepts/layouts': 'Layouts',
+	'guides/imports': 'Archivos estáticos',
+
+	// Features
+	features: 'Funcionalidades',
+	'guides/styling': 'Estilos & CSS',
+
+	// Reference
+	'core-concepts/routing': 'Enrutamiento',
 });
